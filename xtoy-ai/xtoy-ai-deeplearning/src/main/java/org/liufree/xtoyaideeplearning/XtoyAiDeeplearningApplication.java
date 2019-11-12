0ae10ccb86548048240f0b2e35e2b6db3dd039ad
liufree <liufreeo@gmail.com>
2019-11-12 09:38:12 +0800

    s

---
+++ xtoy-ai/xtoy-ai-deeplearning/src/main/java/org/liufree/xtoyaideeplearning/XtoyAiDeeplearningApplication.java
@@ -0,0 +1,13 @@
+package org.liufree.xtoyaideeplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtoyAiDeeplearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XtoyAiDeeplearningApplication.class, args);
+    }
+
+}
---
+++ xtoy-ai/xtoy-ai-deeplearning/src/test/java/org/liufree/xtoyaideeplearning/XtoyAiDeeplearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liufree.xtoyaideeplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtoyAiDeeplearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
